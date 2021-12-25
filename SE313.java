4a9830d7925cc1a9196113eb3d51bf479eeda2d2
Billal490 <75011551+Billal490@users.noreply.github.com>
2021-12-25 23:10:59 +0600

    Update SE313.java

--- SE313.java
+++ SE313.java
@@ -6,6 +6,7 @@ class Main {
     // create an array
     int[] numbers = {3, 9, 5, -5};
     
+    //new author
     // for each loop 
     for (int number: numbers) {
       System.out.println(number);
